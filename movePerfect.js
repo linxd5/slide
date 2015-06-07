0cc5829dc83c15f035e7b140fb2df121264c6e26
linxd5 <164532640@qq.com>
2015-06-07 16:15:27 +0800

    slide

---
+++ movePerfect.js
@@ -0,0 +1,46 @@
+/**
+ * Created by linxd on 2015/6/6.
+ */
+
+
+function getStyle(obj, name) {
+    if (getComputedStyle) {
+        return getComputedStyle(obj, null)[name];
+    } else {
+        return obj.currentStyle[name];
+    }
+}
+
+function startMove(obj, json, fnEnd) {
+    clearInterval(obj.timer);
+    obj.timer=setInterval(function () {
+        var bStop = true;
+        for (var attr in json) {
+            var cur = 0;
+            if (attr == "opacity") {
+                cur = Math.round(parseFloat(getStyle(obj, attr))*100);
+            } else {
+                cur = parseInt(getStyle(obj, attr));
+            }
+            var speed = (json[attr]-cur)/6;
+
+            speed=speed>0? Math.ceil(speed):Math.floor(speed);
+
+            if (cur != json[attr]) {
+                bStop = false;
+            }
+
+            if (attr == "opacity") {
+                obj.style.filter = "alpha(opacity:" + (cur+speed) + ")";
+                obj.style.opacity = (cur+speed)/100;
+            } else {
+                obj.style[attr] = cur + speed + "px";
+            }
+        }
+
+        if (bStop) {
+            clearInterval(obj.timer);
+            if (fnEnd) fnEnd();
+        }
+    }, 30);
+}
